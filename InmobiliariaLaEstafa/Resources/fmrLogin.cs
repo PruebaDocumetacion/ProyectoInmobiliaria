872cd44259e1de899e31b66852f4104f2e45e9dd
Gimena_UNICAH <Gimena_UNICAH@GIMENA>
2018-07-27 10:32:25 -0600

    cambios 1

---
+++ InmobiliariaLaEstafa/Resources/fmrLogin.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conexion
+{
+    public partial class fmrLogin : Form
+    {
+        public fmrLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+            //Eliminar las siguientes dos lineas para validar el login
+            //--¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡
+          // txtUsuario.Text = "InmoAdmin";
+           //txtClave.Text = "LaEstafa";
+
+            string usuario = "InmoAdmin";
+            string clave = "LaEstafa";
+            // valida si el usuario y contraseña estan en lo correcto
+
+            if (txtUsuario.Text != usuario)
+            {
+                errorProvider1.SetError(txtUsuario, "El usuario ingresado no existe");
+                errorProvider1.GetError(txtUsuario);
+            }
+            else if (txtClave.Text != clave)
+            {
+                errorProvider1.SetError(txtClave, "El Clave ingresado es incorrecta");
+                errorProvider1.GetError(txtClave);
+            }
+            else
+            {
+                //llama al formulario del menú
+                frmMenu frm1 = new frmMenu();
+                this.Hide();
+                frm1.ShowDialog();
+            }
+        }
+
+        private void fmrLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ InmobiliariaLaEstafa/Servidor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InmobiliariaLaEstafa
+{
+    public partial class Servidor : Form
+    {
+        public Servidor()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Ingrese Servidor");
+            }
+            else
+            {
+                Global.ip = textBox1.Text;
+                frmLogin s = new frmLogin();
+                
+                MessageBox.Show("Hola");
+                
+            }
+        }
+    }
+}
